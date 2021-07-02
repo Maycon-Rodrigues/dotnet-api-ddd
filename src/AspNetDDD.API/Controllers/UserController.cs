2a0b56ae5d7a42aa4e5f2ced04b76a51474cb6f8
Maycon-Rodrigues <maycon.zng@gmail.com>
2021-07-02 16:21:28 -0300

    create methods cruds

--- src/AspNetDDD.API/Controllers/UserController.cs
+++ src/AspNetDDD.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetDDD.Domain;
 using AspNetDDD.Domain.Interfaces.Service;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,7 +11,6 @@ namespace AspNetDDD.API.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
-
         public UserController(IUserService userService)
         {
             _userService = userService;
@@ -23,6 +23,52 @@ namespace AspNetDDD.API.Controllers
             if (users.Any())
             {
                 return Ok(users);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("users/{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _userService.GetById(id);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("users")]
+        public async Task<IActionResult> CreateUser(UserViewModel userViewModel)
+        {
+            var user = await _userService.Create(userViewModel);
+            if (user != null)
+            {
+                return CreatedAtAction(nameof(GetAllUsers), user);
+            }
+            return NoContent();
+        }
+
+        [HttpPut("users")]
+        public async Task<IActionResult> UpdateUser(UserViewModel userViewModel)
+        {
+            var user = await _userService.GetById(userViewModel.Id);
+            if (user != null)
+            {
+                await _userService.Update(userViewModel);
+                return NoContent();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("users/{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _userService.GetById(id);
+            if (user != null)
+            {
+                await _userService.Delete(user.Id);
+                return NoContent();
             }
             return NotFound();
         }
